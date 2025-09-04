c4e8485e56c27d3dae74c1bac5272bc8bbbbc4c9
Derrick Wong <admin@wongderrick.altostrat.com>
2025-09-04 10:43:21 +0800

    chore: update cucumber timeout

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,12 @@
 module.exports = {
   default: {
-    requireModule: ['ts-node/register'],
+    format: [
+      'progress-bar',
+      'html:cucumber-report.html'
+    ],
+    paths: ['features/**/*.feature'],
     require: ['features/step_definitions/**/*.ts'],
-    format: ['summary', 'progress-bar'],
-    publishQuiet: true,
-    timeout: 10000,
-  },
+    requireModule: ['ts-node/register'],
+    timeout: 2000
+  }
 };
